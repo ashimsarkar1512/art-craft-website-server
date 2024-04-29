65aaf6606cdbe239c27867bee2035e1a4ff3d372
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-04-29 10:10:49 +0600

    email identity and get the value

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/addCraft/:email', async(req,res)=>{
+      console.log(req.params.email);
+      const result=await artCraftCollection.find({email:req.params.email}).toArray();
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
